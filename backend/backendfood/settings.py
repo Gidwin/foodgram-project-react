eba4e8704c910d64598c9f81f6ff885789634c66
Gidwin <gidwin@mail.ru>
2023-03-09 18:30:26 +0300

    try deploy

--- backend/backendfood/settings.py
+++ backend/backendfood/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
 
+
+load_dotenv()
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'y3x2^90k@&-djhyfwz_ci2u)tcuzakvtx+1k(()m)6q+^q&vy='
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='y3x2^90k@&-djhyfwz_ci2u)tcuzakvtx+1k(()m)6q+^q&vy=')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
