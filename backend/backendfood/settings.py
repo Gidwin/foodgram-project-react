59b45fb860bb42da799dd72f510fc795cc53d709
Gidwin <gidwin@mail.ru>
2023-03-09 19:24:52 +0300

    fixmb

--- backend/backendfood/settings.py
+++ backend/backendfood/settings.py
@@ -24,7 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+SECRET_KEY = os.getenv(
+    'SECRET_KEY',
+    default='y3x2^90k@&-djhyfwz_ci2u)tcuzakvtx+1k(()m)6q+^q&vy='
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
